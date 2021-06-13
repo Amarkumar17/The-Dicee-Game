4521e1d0dc8dead95dca22ae4ac930f127fab5cc
Amarkumar17 <84021256+Amarkumar17@users.noreply.github.com>
2021-06-13 13:33:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.ceil(Math.random()*6);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSrc = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSrc);
+
+var randomNumber2 = Math.ceil(Math.random()*6);
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomDiceImage2);
+
+
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw"
+}
